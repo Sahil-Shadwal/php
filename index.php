64ce40d57bfb592f2494f966b38045f25cd946ac
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-07 10:50:30 +0530

    hello php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+
+    
+        <?php
+            $Greetings = "fucking";
+            echo "$Greetings  World!";
+            // echo $Greetings." World!";  // concatination is done by .(period) instead of +(plus)
+        ?>
+
+        <!-- <?php
+            echo "Hello fcking World!";
+        ?> -->
+        <!-- <?php
+            echo "Hello "." World!";  // concatination is done by .(period) instead of +(plus)
+        ?> -->
+    </h1>
+    
+</body>
+</html>
