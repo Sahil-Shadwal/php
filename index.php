78db17a24a19e171efccd0f58b31d3794b210197
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-07 13:21:40 +0530

    if else

--- index.php
+++ index.php
@@ -3,23 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 90vh;
+            margin: 0;
+            font-size: 2rem;
+            font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php 
+        $name = "Dark Matters";
+        $read = true;
+
+        if($read){
+            $message = "You have read $name";
+        }else {
+            $message = "You have not read $name";
+        }
+    ?>
     <h1>
-
-    
-        <?php
-            $Greetings = "fucking";
-            echo "$Greetings  World!";
-            // echo $Greetings." World!";  // concatination is done by .(period) instead of +(plus)
-        ?>
-
-        <!-- <?php
-            echo "Hello fcking World!";
-        ?> -->
-        <!-- <?php
-            echo "Hello "." World!";  // concatination is done by .(period) instead of +(plus)
-        ?> -->
+        <!-- <?php echo $message; ?> -->
+        <?= $message; ?>
     </h1>
     
 </body>
