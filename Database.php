c6f90db83b6a3ebd8c6d0fc1f0006b5260215b72
Sahil Shadwal <sahilshadwal@gmail.com>
2024-08-10 21:47:37 +0530

    optimization

--- Database.php
+++ Database.php
@@ -6,8 +6,19 @@ class Database
 
     public function __construct()
     {
-        $dns = "mysql:host=localhost;port=3306;user=root;password=sexy;dbname=myass;charset=utf8mb4";
-        $this->connection = new PDO($dns);
+        $config = [
+            'host' => 'localhost',
+            'port' => 3306,
+            'dbname' => 'myass',
+            'charset' => 'utf8mb4'
+        ];
+
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, 'root', 'sexy', [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
     public function query($query)
     {
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require 'Database.php';
 
 $db = new Database();
 
-$posts = $db->query('Select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query('Select * from posts')->fetchAll(); //::scope resolution operator -> it gives us the access to the static and constant that was defined in the class
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
