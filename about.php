dae64a3e217f220a3bbf52ae2f9930209467568d
Sahil Shadwal <sahilshadwal@gmail.com>
2024-08-10 09:55:13 +0530

    rrr

--- about.php
+++ about.php
@@ -1,35 +1,6 @@
 <?php
 
 
-$books = [
-    [
-
-        'name' => 'Do Androids Dream of Electric Sheep?',
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'https://shadwal.site'
-    
-    ],
-    [
-
-        'name' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'https://shadwal.site'
-    
-    ],
-    [
-
-        'name' => 'The Martian',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'https://shadwal.site'
-    
-    ],
-];
-
-$filteredBooks = array_filter($books,function($book) {
-    return $book['author'] === 'Andy Weir';
-});
+$heading = 'About Us';
 
 require 'views/about.view.php';
