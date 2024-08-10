452481abfd000063494152cede2547f9b528f8db
Sahil Shadwal <sahilshadwal@gmail.com>
2024-08-10 23:08:36 +0530

    f

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ $config = require 'config.php';
 
 $db = new Database($config['database']);
 
-$id = $_GET['id'];
-$query = "select * from posts where id = ?";
+$id = $_GET['id']; //never inline user data in query string
+$query = "select * from posts where id = :id";
 
-$posts = $db->query($query, [$id])->fetch(); //::scope resolution operator -> it gives us the access to the static and constant that was defined in the class
+$posts = $db->query($query, [':id' => $id])->fetch(); //::scope resolution operator -> it gives us the access to the static and constant that was defined in the class
 
 dd($posts);
