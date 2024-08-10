34a7e01eacf5f10772f84cfe3f64ef5b8a5a6a9a
Sahil Shadwal <sahilshadwal@gmail.com>
2024-08-10 15:16:09 +0530

    classes in php 101

--- index.php
+++ index.php
@@ -3,4 +3,24 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+class Person
+{
+    public $name;
+    public $age;
+
+    public function breathe()
+    {
+        // echo "breathing fire";
+        echo $this->name . 'is breathing';
+    }
+}
+
+$person = new Person();
+
+$person->name = "Jack Sparrow";
+$person->age = 21;
+
+// dd($person->breathe());
+$person->breathe();
