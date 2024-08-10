95a183a2fea9fecbbe0767d51b8775965462cfff
Sahil Shadwal <sahilshadwal@gmail.com>
2024-08-10 14:26:01 +0530

    routing and 404

--- index.php
+++ index.php
@@ -3,14 +3,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if ($uri === '/') {
-    require 'controllers/index.php';
-}
-if ($uri === '/about') {
-    require 'controllers/about.php';
-}
-if ($uri === '/contact') {
-    require 'controllers/contact.php';
-}
+require 'router.php';
